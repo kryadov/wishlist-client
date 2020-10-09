c43020d3391ff26f912f79daa11550a90ae88cf1
Konstantin Ryadov <kryadov@mail.ru>
2020-10-10 00:54:59 +0300

    updated tests

--- test/components/WishlistItem.test.tsx
+++ test/components/WishlistItem.test.tsx
@@ -1,10 +1,13 @@
+import { assert } from 'console';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import WishlistItem from '../../src/components/WishlistItem';
 
-test('Link changes the class when hovered', () => {
-    const props = {id:1, name: "!", description: "2", credits: 1.0};
+test('Test Wishlist Component', () => {
+    const props = {id:1, name: "!123-1.0", description: "2", credits: 1.0};
     const component = renderer.create(
         <WishlistItem key="1" wishlistItem={props}/>
       );
+    const json = component.toJSON();
+    expect(JSON.stringify(component)).toContain("!123-1.0");
 });
